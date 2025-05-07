46622daeb95733a6ce97ab8ee5c856bd1b0a27c4
javiercolv <javiercolv@gmail.com>
2025-05-07 13:16:27 -0600

    initial commit

---
+++ src/main.js
@@ -0,0 +1,36 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vite.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
+document.addEventListener('scroll', () => {
+  const welcomeSection = document.querySelector('.welcome');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 50) {
+    welcomeSection.style.height = '0'; // Collapse completely
+    welcomeSection.style.overflow = 'hidden'; // Hide content when collapsed
+  } else {
+    welcomeSection.style.height = '100vh'; // Restore to full height
+    welcomeSection.style.overflow = 'visible'; // Show content when restored
+  }
+});
+
+setupCounter(document.querySelector('#counter'))
